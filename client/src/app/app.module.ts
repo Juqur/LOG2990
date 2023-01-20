3cfb7de1c0edb14c8cc7ba77db58eac47cfccd5f
Lfsta <76858115+Lfsta@users.noreply.github.com>
2023-01-20 16:20:16 -0500

    fixed merge issue

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from '@app/components/card/card.component';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
 import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
@@ -12,9 +13,8 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { SelectionPageComponent } from '@app/pages/selection-page/selection-page.component';
-import { CardComponent } from '@app/components/card/card.component';
-import { GameTimerComponent } from './components/game-timer/game-timer.component';
 import { GameChatComponent } from './components/game-chat/game-chat.component';
+import { GameTimerComponent } from './components/game-timer/game-timer.component';
 import { ImageDifferenceComponent } from './components/image-difference/image-difference.component';
 
 /**
