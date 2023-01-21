455ae660d7d779d17d35e1f4f55528cc8afa7f1f
A123wq123 <charles.degrandpre@polymtl.ca>
2023-01-20 20:39:06 -0500

    feature/VueDeJeuEnSolo/added logic to game-timer

--- client/src/app/components/game-timer/game-timer.component.ts
+++ client/src/app/components/game-timer/game-timer.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-game-timer',
-  templateUrl: './game-timer.component.html',
-  styleUrls: ['./game-timer.component.scss']
+    selector: 'app-game-timer',
+    templateUrl: './game-timer.component.html',
+    styleUrls: ['./game-timer.component.scss'],
 })
 export class GameTimerComponent implements OnInit {
+    @Input() isCountDown: boolean;
+    @Input() gameLength: number;
 
-  constructor() { }
+    startTime: number;
+    gameTime: number = 1;
+    interval: ReturnType<typeof setTimeout>;
 
-  ngOnInit(): void {
-  }
+    timer() {
+        if (this.isCountDown) {
+            this.downTimer();
+        } else {
+            this.upTimer();
+        }
+    }
 
+    downTimer() {
+        this.interval = setInterval(() => {
+            if (this.gameTime > 0) {
+                const currentTime: number = new Date().getSeconds();
+                this.gameTime = this.gameLength - (currentTime - this.startTime);
+            } else {
+                // TODO
+                // Send message that timer has ended.
+            }
+        }, 1000);
+    }
+
+    upTimer() {
+        this.interval = setInterval(() => {
+            if (this.gameTime < this.gameLength) {
+                const currentTime: number = new Date().getSeconds();
+                this.gameTime = currentTime - this.startTime;
+            } else {
+                // TODO
+                // Send message that timer has ended.
+            }
+        }, 1000);
+    }
+
+    ngOnInit(): void {
+        this.startTime = new Date().getSeconds();
+        this.timer();
+    }
 }
