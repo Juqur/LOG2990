013a8be3e476f51d4b6e84339138d6a584cbd216
Primexus <pierre.tran@ymail.com>
2023-01-23 23:23:28 -0500

    Added play audio

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -7,15 +7,25 @@ import { Component } from '@angular/core';
 export class MainPageComponent {
     icon: string = 'volume_up';
     isCreditsClosed: boolean = true;
+
     volumeOnClick() {
+        this.playAudio();
         this.icon = this.icon === 'volume_up' ? 'volume_off' : 'volume_up';
     }
 
     creditsOnClick() {
+        this.playAudio();
         const credits = document.getElementById('credits');
         if (!credits) {
             return;
         }
         credits.style.display = credits.style.display === 'block' ? 'none' : 'block';
+    }
+
+    playAudio() {
+        const audio = new Audio();
+        audio.src = './assets/audio/click.mp3';
+        audio.load();
+        audio.play();
     }
 }
