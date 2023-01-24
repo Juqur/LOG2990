3cd6e9a4ec3a99c6e50816490a073a84ca4fdaa2
Primexus <pierre.tran@ymail.com>
2023-01-24 13:59:29 -0500

    Created audio as an attribute of main-page component.

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,27 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
     selector: 'app-main-page',
     templateUrl: './main-page.component.html',
     styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
     icon: string = 'volume_up';
     isCreditsClosed: boolean = true;
-
+    audio: HTMLAudioElement = new Audio('./assets/audio/click.mp3');
     constructor(private router: Router) {}
+
+    ngOnInit(): void {
+        this.audio.load();
+    }
 
     startGameOnClick() {
         this.router.navigate(['/game']);
     }
 
     volumeOnClick() {
-        this.playAudio();
+        this.audio.play();
         this.icon = this.icon === 'volume_up' ? 'volume_off' : 'volume_up';
     }
 
     creditsOnClick() {
-        this.playAudio();
+        this.audio.play();
         const credits = document.getElementById('credits');
         if (!credits) {
             return;
@@ -29,10 +33,10 @@ export class MainPageComponent {
         credits.style.display = credits.style.display === 'block' ? 'none' : 'block';
     }
 
-    playAudio() {
-        const audio = new Audio();
-        audio.src = './assets/audio/click.mp3';
-        audio.load();
-        audio.play();
-    }
+    // playAudio() {
+    //     const audio = new Audio();
+    //     audio.src = './assets/audio/click.mp3';
+    //     audio.load();
+    //     audio.play();
+    // }
 }
