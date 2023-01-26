bb175b69d9b3c7d5328a694cabc87a84ad2bd351
Lfsta <76858115+Lfsta@users.noreply.github.com>
2023-01-25 19:15:37 -0500

    fixed one lint issue and commit because i cant switch branches...

--- client/src/app/components/card/card.component.ts
+++ client/src/app/components/card/card.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Level } from '@app/levels';
 
-
 // import { CarouselComponent } from '../carousel/carousel.component';
 @Component({
     selector: 'app-card',
