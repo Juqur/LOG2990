5de171e7219fc1172b8322e3cac0472d1bd4f722
JunaidJunaidJunaidJunaid <97924533+JunaidJunaidJunaidJunaid@users.noreply.github.com>
2023-01-25 19:01:59 -0500

    Removed all audio source, too much of a hassle

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,37 +1,38 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
     selector: 'app-main-page',
     templateUrl: './main-page.component.html',
     styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent implements OnDestroy {
-    icon: string = 'volume_up';
-    audio: HTMLAudioElement;
+export class MainPageComponent {
+    icon: string = 'volume_off';
     constructor(private router: Router) {
-        this.audio = new Audio();
-        this.audio.src = './assets/audio/soundtrack.mp3';
-        this.audio.addEventListener('loadeddata', () => {
-            this.audio.play();
-        });
+        // this.audio = new Audio();
+        // this.audio.src = './assets/audio/soundtrack.mp3';
+        // this.audio.muted = true;
+        // this.audio.load();
     }
 
-    ngOnDestroy() {
-        this.audio.pause();
-    }
+    // ngOnDestroy() {
+    //     this.audio.pause();
+    // }
 
     startGameOnClick() {
         this.router.navigate(['/game']);
     }
 
     volumeOnClick() {
-        this.playAudio();
-        this.audio.muted = !this.audio.muted;
+        // this.playAudio();
+        // if (this.audio.paused) {
+        //     this.audio.play();
+        // }
+        // this.audio.muted = !this.audio.muted;
         this.icon = this.icon === 'volume_up' ? 'volume_off' : 'volume_up';
     }
 
     creditsOnClick() {
-        this.playAudio();
+        // this.playAudio();
         const credits = document.getElementById('credits');
         if (!credits) {
             return;
@@ -39,11 +40,11 @@ export class MainPageComponent implements OnDestroy {
         credits.style.display = credits.style.display === 'block' ? 'none' : 'block';
     }
 
-    playAudio() {
-        const audio = new Audio('./assets/audio/click.mp3');
-        audio.load();
-        if (audio.paused) {
-            audio.play();
-        }
-    }
+    // playAudio() {
+    //     const audio = new Audio('./assets/audio/click.mp3');
+    //     audio.load();
+    //     if (audio.paused) {
+    //         audio.play();
+    //     }
+    // }
 }
