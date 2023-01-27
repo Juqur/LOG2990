3582799080db9fbc7709a3cae1770fbd901d494a
JunaidJunaidJunaidJunaid <97924533+JunaidJunaidJunaidJunaid@users.noreply.github.com>
2023-01-27 16:38:18 -0500

    Radius algorithm implemented, still bugged however, half circle around pixel instead of full

--- client/src/app/pages/test-diff.component.ts
+++ client/src/app/pages/test-diff.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DifferenceDetectorService } from '../services/difference-detector.service';
 
 @Component({
     selector: 'app-test-diff',
@@ -9,6 +10,7 @@ export class TestDiffComponent {
     defaultImage: File | null = null;
     diffImage: File | null = null;
     radius = 3;
+    constructor(private differenceDetectorService: DifferenceDetectorService) {}
 
     defaultImageSelector(event: Event) {
         const target = event.target as HTMLInputElement;
@@ -23,5 +25,32 @@ export class TestDiffComponent {
             return;
         }
         this.diffImage = target.files[0];
+    }
+    detectDifference() {
+        if (!this.defaultImage || !this.diffImage) {
+            return;
+        }
+        const defaultCanvas = document.createElement('canvas').getContext('2d');
+        const diffCanvas = document.createElement('canvas').getContext('2d');
+        document.body.appendChild(defaultCanvas?.canvas as HTMLCanvasElement);
+        document.body.appendChild(diffCanvas?.canvas as HTMLCanvasElement);
+        const image1 = new Image();
+        const image2 = new Image();
+        image1.src = URL.createObjectURL(this.defaultImage);
+        image2.src = URL.createObjectURL(this.diffImage);
+        image1.onload = () => {
+            image2.onload = () => {
+                if (!defaultCanvas || !diffCanvas) {
+                    return;
+                }
+                defaultCanvas.canvas.width = image1.width;
+                defaultCanvas.canvas.height = image1.height;
+                diffCanvas.canvas.width = image1.width;
+                diffCanvas.canvas.height = image1.height;
+                defaultCanvas?.drawImage(image1, 0, 0);
+                diffCanvas?.drawImage(image2, 0, 0);
+                this.differenceDetectorService.differentDectection(defaultCanvas, diffCanvas, this.radius.toString());
+            };
+        };
     }
 }
--- client/src/app/services/difference-detector.service.ts
+++ client/src/app/services/difference-detector.service.ts
@@ -3,4 +3,57 @@ import { Injectable } from '@angular/core';
 @Injectable({
     providedIn: 'root',
 })
-export class DifferenceDetectorService {}
+export class DifferenceDetectorService {
+    differentDectection(defaultImage: CanvasRenderingContext2D, secondImage: CanvasRenderingContext2D, radius: string) {
+        const radiusNumber = Number(radius);
+        if (radiusNumber <= 0) {
+            return;
+        }
+        const defaultImageData = defaultImage.getImageData(0, 0, defaultImage.canvas.width, defaultImage.canvas.height);
+        const secondImageData = secondImage.getImageData(0, 0, secondImage.canvas.width, secondImage.canvas.height);
+        const differenceImageData = defaultImage.createImageData(defaultImage.canvas.width, defaultImage.canvas.height);
+        const defaultData = defaultImageData.data;
+        const secondData = secondImageData.data;
+        const differenceData = differenceImageData.data;
+
+        // r,g,b,a
+
+        for (let i = 0; i < defaultData.length; i += 4) {
+            const r = defaultData[i];
+            const g = defaultData[i + 1];
+            const b = defaultData[i + 2];
+            const r2 = secondData[i];
+            const g2 = secondData[i + 1];
+            const b2 = secondData[i + 2];
+            if (r - r2 > 0 || g - g2 > 0 || b - b2 > 0) {
+                for (let j = -radiusNumber; j < radiusNumber; j++) {
+                    for (let k = -radiusNumber; k < radiusNumber; k++) {
+                        const pixelPosition = j * 4 + k * 4 * defaultImage.canvas.width + i;
+                        if (pixelPosition >= 0 && pixelPosition < defaultData.length && Math.pow(j, 2) + Math.pow(k, 2) < Math.pow(radiusNumber, 2)) {
+                            differenceData[pixelPosition] = 0;
+                            differenceData[pixelPosition + 1] = 0;
+                            differenceData[pixelPosition + 2] = 0;
+                            differenceData[pixelPosition + 3] = 255;
+                            if (j === 0 && k === 0) {
+                                differenceData[pixelPosition] = 255;
+                                differenceData[pixelPosition + 1] = 0;
+                                differenceData[pixelPosition + 2] = 0;
+                                differenceData[pixelPosition + 3] = 255;
+                            }
+                        }
+                    }
+                }
+            } else {
+                differenceData[i + 3] = 0;
+            }
+        }
+        const differenceCanvas = document.createElement('canvas').getContext('2d');
+        if (!differenceCanvas) {
+            return;
+        }
+        differenceCanvas.canvas.width = defaultImage.canvas.width;
+        differenceCanvas.canvas.height = defaultImage.canvas.width;
+        differenceCanvas.putImageData(differenceImageData, 0, 0);
+        document.body.appendChild(differenceCanvas.canvas);
+    }
+}
