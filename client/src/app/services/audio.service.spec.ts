d245ad30ec9c603ec5ac34f49ab246cb450486b5
Primexus <pierre.tran@ymail.com>
2023-01-27 16:15:37 -0500

    Covered all lines.

--- client/src/app/services/audio.service.spec.ts
+++ client/src/app/services/audio.service.spec.ts
@@ -13,16 +13,23 @@ describe('AudioService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
-    it('should create an audio HTML element', () => {
+
+    it('create should create an audio HTML element', () => {
         const audio = service.create('./assets/audio/click.mp3');
         expect(audio).toBeInstanceOf(HTMLAudioElement);
     });
-    it('should play an audio HTML element', () => {
+
+    it('play should play the audio and the autoplay is allowed', () => {
         const audio = service.create('./assets/audio/click.mp3');
-        const spyAudioPlay = spyOn(audio, 'play');
+        const spyAudioPlay = spyOn(audio, 'play').and.returnValue(Promise.resolve());
         service.play(audio);
         expect(spyAudioPlay).toHaveBeenCalled();
     });
+
+    it('play should not play the audio if it is undefined and the autoplay is denied', () => {
+        service.play(undefined);
+    });
+
     it('playSound should call create and play', () => {
         const spyCreate = spyOn(service, 'create');
         const spyPlay = spyOn(service, 'play');
@@ -30,10 +37,18 @@ describe('AudioService', () => {
         expect(spyCreate).toHaveBeenCalled();
         expect(spyPlay).toHaveBeenCalled();
     });
-    it('should mute an audio HTML element', () => {
-        const audio = service.create('./assets/audio/click.mp3');
+
+    it('mute should mute a specific audio HTML element', () => {
+        const audio = service.create('./assets/audio/soundtrack.mp3');
         service.play(audio);
         service.mute(audio);
         expect(audio.muted).toBeTruthy();
+    });
+
+    it('mute should mute the soundtrack', () => {
+        service.soundtrack = service.create('./assets/audio/soundtrack.mp3');
+        service.play(service.soundtrack);
+        service.mute();
+        expect(service.soundtrack.muted).toBeTruthy();
     });
 });
