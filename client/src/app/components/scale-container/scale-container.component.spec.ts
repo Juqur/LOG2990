ee72091628104102a106ac81fb6ebaa5997a3eb9
Primexus <pierre.tran@ymail.com>
2023-02-01 18:46:29 -0500

    Implemented tests.

--- client/src/app/components/scale-container/scale-container.component.spec.ts
+++ client/src/app/components/scale-container/scale-container.component.spec.ts
@@ -1,3 +1,4 @@
+import { ElementRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScaleContainerComponent } from './scale-container.component';
@@ -18,5 +19,30 @@ describe('ScaleContainerComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('resizeContainer should be called on init', () => {
+        const spy = spyOn(component, 'resizeContainer');
+        component.ngOnInit();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('resizeContainer should calculate the scale correctly', () => {
+        const expectScale = 0.4;
+        component.screen = {
+            nativeElement: {
+                offsetWidth: 768,
+                offsetHeight: 926,
+            },
+        } as ElementRef;
+        component.container = {
+            nativeElement: {
+                offsetWidth: 1920,
+                offsetHeight: 1080,
+            },
+        } as ElementRef;
+
+        component.resizeContainer();
+        expect(component.scale).toEqual(expectScale);
     });
 });
