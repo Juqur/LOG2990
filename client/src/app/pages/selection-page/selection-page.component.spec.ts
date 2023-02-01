ed23547f668b695657127165b869edc2babe94bd
Lfsta <76858115+Lfsta@users.noreply.github.com>
2023-02-01 17:55:41 -0500

    coverage 100% taht dont mean its good tho

--- client/src/app/pages/selection-page/selection-page.component.spec.ts
+++ client/src/app/pages/selection-page/selection-page.component.spec.ts
@@ -10,7 +10,9 @@ describe('SelectionPageComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [SelectionPageComponent],
         }).compileComponents();
+    });
 
+    beforeEach(() => {
         fixture = TestBed.createComponent(SelectionPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -18,5 +20,22 @@ describe('SelectionPageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('levels should not be empty', () => {
+        expect(component.levels.length).toBeGreaterThan(0);
+    });
+
+    it('next page should increment the current page', () => {
+        const tempPage = component.currentPage;
+        component.nextPage();
+        expect(component.currentPage).toEqual(tempPage + 1);
+    });
+
+    it('previous pages= should increment the current page', () => {
+        component.currentPage = component.lastPage;
+        const tempPage = component.currentPage;
+        component.previousPage();
+        expect(component.currentPage).toEqual(tempPage - 1);
     });
 });
