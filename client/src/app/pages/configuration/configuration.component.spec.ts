45568fb4b0fa7aa5a39975ce92f3b58c46c50676
LFSTA <algorix1217@gmail.com>
2023-02-01 21:31:15 -0500

    Revert "basically copy pasta from pageDeSelection"
    This reverts commit fe71bc8083620b07f54610a76607626dfb668f1c.

--- client/src/app/pages/configuration/configuration.component.spec.ts
+++ client/src/app/pages/configuration/configuration.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { levels } from '@app/levels';
-import { Constants } from '@common/constants';
+
 import { ConfigurationComponent } from './configuration.component';
 
 describe('ConfigurationComponent', () => {
@@ -19,42 +18,5 @@ describe('ConfigurationComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
-    });
-
-    // Il devrait toujours exister une partie dans la base de donnée du serveur
-    it('levels should not be empty', () => {
-        expect(component.levels.length).toBeGreaterThan(0);
-    });
-
-    it('Should load up to 4 cards', () => {
-        const cards = document.getElementsByClassName('card');
-        if (levels.length < Constants.levelsPerPage) {
-            expect(cards.length).toEqual(levels.length);
-        }
-        expect(cards.length).toBeLessThanOrEqual(Constants.levelsPerPage);
-    });
-
-    it('nextPage() should increment the current page', () => {
-        const tempPage = component.currentPage;
-        component.nextPage();
-        expect(component.currentPage).toEqual(tempPage + 1);
-    });
-
-    it('previousPage() should decrement the current page', () => {
-        component.currentPage = component.lastPage;
-        const tempPage = component.currentPage;
-        component.previousPage();
-        expect(component.currentPage).toEqual(tempPage - 1);
-    });
-
-    it('arrow_back button should be disabled if at the first page', () => {
-        expect(document.getElementById('arrow_back')).toBeInstanceOf(HTMLButtonElement);
-        expect((document.getElementById('arrow_back') as HTMLButtonElement).disabled).toBeTruthy();
-    });
-
-    it('arrow_back button should be disabled if at the first page', () => {
-        component.currentPage = component.lastPage;
-        expect(document.getElementById('arrow_back')).toBeInstanceOf(HTMLButtonElement);
-        expect((document.getElementById('arrow_back') as HTMLButtonElement).disabled).toBeTruthy();
     });
 });
