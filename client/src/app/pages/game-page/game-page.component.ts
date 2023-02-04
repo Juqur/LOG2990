01fbc4dc7a3a158e105067f65387dc3e434169c0
A123wq123 <charles.degrandpre@polymtl.ca>
2023-02-03 22:53:22 -0500

    feature/modeClassiqueEnSolo/removed rolution with only services in favor of component

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { MouseService } from '@app/services/mouse.service';
 
 @Component({
     selector: 'app-game-page',
     templateUrl: './game-page.component.html',
     styleUrls: ['./game-page.component.scss'],
 })
-export class GamePageComponent {
-    constructor(private service: MouseService) {}
-
-    getDifferenceCounter(): number {
-        return this.service.differenceCounter;
-    }
-}
+export class GamePageComponent {}
