bf54b3575f9fb7ed746fc1717814e295cea95554
XxshimxX <89857605+XxshimxX@users.noreply.github.com>
2023-02-08 18:20:35 -0500

    Ajout de quelques tests

--- client/src/app/pages/creation/creation.component.spec.ts
+++ client/src/app/pages/creation/creation.component.spec.ts
@@ -1,17 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CanvasSharingService } from '@app/services/canvas-sharing.service';
 
 import { CreationComponent } from './creation.component';
 
 describe('CreationComponent', () => {
     let component: CreationComponent;
+    let canvasSharingService: CanvasSharingService;
     let fixture: ComponentFixture<CreationComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [CreationComponent],
+            providers: [CanvasSharingService],
         }).compileComponents();
 
         fixture = TestBed.createComponent(CreationComponent);
+        canvasSharingService = TestBed.inject(CanvasSharingService);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
@@ -19,4 +23,67 @@ describe('CreationComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should set defaultImage with selected file', () => {
+        const mockFile = new File([''], 'mock.jpg');
+        const mockEvent = { target: { files: [mockFile] } } as any;
+        component.defaultImageSelector(mockEvent);
+        expect(component.defaultImage).toEqual(mockFile);
+    });
+
+    it('defaultImageSelector should call showDefaultImage', () => {
+        const mockFile = new File([''], 'mock.jpg');
+        const mockFileInput = { target: { files: [mockFile] } } as any;
+        const spy = spyOn(component, 'showDefaultImage');
+        component.defaultImageSelector(mockFileInput);
+        expect(spy).toHaveBeenCalled();
+    });
+    it('diffImageSelector should call showDiffImage', () => {
+        const mockFile = new File([''], 'mock.jpg');
+        const mockFileInput = { target: { files: [mockFile] } } as any;
+        const spy = spyOn(component, 'showDiffImage');
+        component.diffImageSelector(mockFileInput);
+        expect(spy).toHaveBeenCalled();
+
+    });
+    it('bothImagesSelector should call both showDefaultImage and showDiffImage', () => {
+        const mockFile = new File([''], 'mock.jpg');
+        const mockFileInput = { target: { files: [mockFile] } } as any;
+        const defaultSpy = spyOn(component, 'showDefaultImage');
+        const diffSpy = spyOn(component, 'showDiffImage');
+        component.bothImagesSelector(mockFileInput);
+        expect(defaultSpy).toHaveBeenCalled();
+        expect(diffSpy).toHaveBeenCalled();
+    });
+
+    it('showDefaultImage should show default image on canvas', () => {
+        component.defaultCanvasCtx = document.createElement('canvas').getContext('2d');
+        canvasSharingService.setDefaultCanvasRef(component.defaultCanvasCtx?.canvas as HTMLCanvasElement);
+        const mockFile = new File([''], 'mock.jpg');
+        component.defaultImage = mockFile;
+        component.showDefaultImage();
+        expect(canvasSharingService.defaultCanvasRef.width).toBeGreaterThan(0);
+        expect(canvasSharingService.defaultCanvasRef.height).toBeGreaterThan(0);
+    });
+
+    it('showDiffImage should show diff image on canvas', () => {
+        component.diffCanvasCtx = document.createElement('canvas').getContext('2d');
+        canvasSharingService.setDiffCanvasRef(component.diffCanvasCtx?.canvas as HTMLCanvasElement);
+        const mockFile = new File([''], 'mock.jpg');
+        component.diffImage = mockFile;
+        component.showDiffImage();
+        expect(canvasSharingService.diffCanvasRef.width).toBeGreaterThan(0);
+        expect(canvasSharingService.diffCanvasRef.height).toBeGreaterThan(0);
+    });
+
+    /*it('resetDefault should reset default canva', () => {
+        component.defaultCanvasCtx = document.createElement('canvas').getContext('2d');
+        canvasSharingService.setDefaultCanvasRef(component.defaultCanvasCtx?.canvas as HTMLCanvasElement);
+        const mockFile = new File([''], 'mock.jpg');
+        component.defaultImage = mockFile;
+        component.showDefaultImage();
+        component.resetDefault();
+    });*/
+
+
 });
