86f4cef4bf30948981c59f89da64d6e63383f9c4
Lfsta <76858115+Lfsta@users.noreply.github.com>
2023-02-10 15:05:49 -0500

    work in progress

--- server/app/services/image/image.service.ts
+++ server/app/services/image/image.service.ts
@@ -4,6 +4,8 @@ import { promises as fs } from 'fs';
 @Injectable()
 export class ImageService {
     readonly path: string = '../server/assets/Data/';
+    readonly pathDifference: string = '../server/assets/Difference/';
+
     async getCardData(): Promise<unknown[]> {
         const files = await fs.readdir(this.path);
         const jsonFiles = files.filter((file) => file.endsWith('.json'));
@@ -13,5 +15,13 @@ export class ImageService {
             return JSON.parse(fileContents);
         });
         return Promise.all(promises);
+    }
+
+    async getDifference(name: string, diffID: number): Promise<number[]> {
+        let allDifferences: number[][] = [];
+        const promises = await fs.readFile(this.pathDifference + name);
+        allDifferences = JSON.parse(promises.toString()) as number[][];
+
+        return allDifferences[diffID];
     }
 }
