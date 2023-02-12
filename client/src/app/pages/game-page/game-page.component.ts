8e14a1117340f8b2a2240c8578c82442b6845b40
A123wq123 <charles.degrandpre@polymtl.ca>
2023-02-12 13:02:05 -0500

    feature/modeClassiqueEnSolo/fixed no return value bug and cleaned code

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,25 +1,8 @@
 import { Component } from '@angular/core';
-import { DialogData, PopUpServiceService } from '@app/services/pop-up-service.service';
-import { Constants } from '@common/constants';
 
 @Component({
     selector: 'app-game-page',
     templateUrl: './game-page.component.html',
     styleUrls: ['./game-page.component.scss'],
 })
-export class GamePageComponent {
-    bodyText: string = 'Test text';
-    data: DialogData = {
-        textToSend: 'Hello',
-        inputData: {
-            inputLabel: 'Name',
-            submitFunction: (value) => {
-                if (value.length < Constants.ten) return true;
-                return false;
-            },
-            returnValue: 'No Value Given',
-        },
-        closeButtonMessage: 'Return to selection page',
-    };
-    constructor(public service: PopUpServiceService) {}
-}
+export class GamePageComponent {}
