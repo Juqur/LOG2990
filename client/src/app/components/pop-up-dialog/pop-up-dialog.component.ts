52179ddf839e03ae33065868607a008d14b3ad1a
A123wq123 <charles.degrandpre@polymtl.ca>
2023-02-12 09:50:13 -0500

    feature/modeClassiqueEnSolo/generated pop up component

---
+++ client/src/app/components/pop-up-dialog/pop-up-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up-dialog',
+  templateUrl: './pop-up-dialog.component.html',
+  styleUrls: ['./pop-up-dialog.component.scss']
+})
+export class PopUpDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
