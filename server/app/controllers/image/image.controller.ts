44fbf576b9397598ea18bf11648a7fc4c8283c7d
Haywire-d <79108425+Haywire-d@users.noreply.github.com>
2023-02-13 01:00:28 -0500

    Changed name for folder

--- server/app/controllers/image/image.controller.ts
+++ server/app/controllers/image/image.controller.ts
@@ -2,7 +2,7 @@ import { Message } from '@app/model/schema/message.schema';
 import { ImageService } from '@app/services/image/image.service';
 import { Controller, Get, Body, Post } from '@nestjs/common';
 import { ApiBody, ApiOkResponse } from '@nestjs/swagger';
-import { levels } from '@app/../assets/data/level';
+import { levels } from 'assets/data/level';
 
 @Controller('image')
 export class ImageController {
