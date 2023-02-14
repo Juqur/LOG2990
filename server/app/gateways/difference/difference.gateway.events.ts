90682b13dd67aab4de4b777c8850eb63fb84ae88
A123wq123 <charles.degrandpre@polymtl.ca>
2023-02-14 09:17:40 -0500

    feature/server/added receive click event

---
+++ server/app/gateways/difference/difference.gateway.events.ts
@@ -0,0 +1,3 @@
+export enum DifferenceEvents {
+    ReceiveClick = 'receiveClick',
+}
--- server/app/gateways/difference/difference.gateway.spec.ts
+++ server/app/gateways/difference/difference.gateway.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DifferenceGateway } from './difference.gateway';
 
 describe('DifferenceGateway', () => {
-  let gateway: DifferenceGateway;
+    let gateway: DifferenceGateway;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [DifferenceGateway],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [DifferenceGateway],
+        }).compile();
 
-    gateway = module.get<DifferenceGateway>(DifferenceGateway);
-  });
+        gateway = module.get<DifferenceGateway>(DifferenceGateway);
+    });
 
-  it('should be defined', () => {
-    expect(gateway).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(gateway).toBeDefined();
+    });
 });
