ceb1883ca3976b93f8350516c46221f467281173
A123wq123 <charles.degrandpre@polymtl.ca>
2023-02-14 23:12:59 -0500

    added 100% code coverage"

--- server/app/providers/image.service.provider.ts
+++
@@ -1,9 +0,0 @@
-import { ImageService } from '@app/services/image/image.service';
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ImageServiceProvider {
-    create(): ImageService {
-        return new ImageService();
-    }
-}
