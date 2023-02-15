96b628a2394709ece79d1f00a87432e87cfdf6ab
JunaidJunaidJunaidJunaid <97924533+JunaidJunaidJunaidJunaid@users.noreply.github.com>
2023-02-14 21:22:45 -0500

    Fixed a couple tests

--- client/src/app/components/game-timer/game-timer.component.spec.ts
+++ client/src/app/components/game-timer/game-timer.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { SocketHandler } from '@app/services/socket-handler.service';
 import { Constants } from '@common/constants';
 import { GameTimerComponent } from './game-timer.component';
@@ -11,6 +12,7 @@ describe('GameTimerComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [GameTimerComponent],
             providers: [SocketHandler],
+            imports: [RouterTestingModule],
         }).compileComponents();
     });
 
