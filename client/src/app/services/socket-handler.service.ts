9b4619202ca3704703e8fbb21b04f6a0337a3853
Primexus <pierre.tran@ymail.com>
2023-02-17 13:29:41 -0500

    Fixed sockethandler.

--- client/src/app/services/socket-handler.service.ts
+++ client/src/app/services/socket-handler.service.ts
@@ -9,6 +9,7 @@ export enum Gateways {
     Timer = 'timer',
     Chat = 'chat',
 }
+
 @Injectable({
     providedIn: 'root',
 })
@@ -23,6 +24,39 @@ export class SocketHandler {
     socketChat: Socket;
 
     /**
+     * Gets the socket of its kind, according to the given gateway.
+     *
+     * @param type The socket's gateway.
+     * @returns The socket of the given gateway.
+     */
+    getSocket(type: Gateways): Socket {
+        switch (type) {
+            case Gateways.Timer:
+                return this.socketTimer;
+            case Gateways.Chat:
+                return this.socketChat;
+        }
+    }
+
+    /**
+     * Sets the socket of its kind to an existing socket.
+     *
+     * @param type The socket's gateway.
+     * @param socket The socket to set.
+     * @returns The socket of the given gateway.
+     */
+    setSocket(type: Gateways, socket: Socket): void {
+        switch (type) {
+            case Gateways.Timer:
+                this.socketTimer = socket;
+                break;
+            case Gateways.Chat:
+                this.socketChat = socket;
+                break;
+        }
+    }
+
+    /**
      * This method verifies if a socket is connected at the given gateway.
      * It first checks if we created a socket of a given gateway and if that socket is currently
      * connected.
@@ -31,51 +65,29 @@ export class SocketHandler {
      * @returns a boolean indicating if a socket is alive at a given gateway.
      */
     isSocketAlive(type: Gateways): boolean {
-        switch (type) {
-            case Gateways.Timer:
-                return this.socketTimer && this.socketTimer.connected;
-            case Gateways.Chat:
-                return this.socketChat && this.socketChat.connected;
-        }
+        const socket = this.getSocket(type);
+        return socket && socket.connected;
     }
 
     /**
      * Connects a socket to a given gateway.
+     * URI : Combination of server.url and the gateway type.
+     * Transport method specified as websocket.
+     * Upgrade to a better transport method as false.
      *
-     * @param type the gateway to connect to.
+     * @param type The gateway to connect to.
      */
     connect(type: Gateways) {
-        switch (type) {
-            case Gateways.Timer:
-                /**
-                 * The following line does this: it tries to create a socket connected to the server
-                 * at the given url, which in our case is a combination of server.url and the gateway type.
-                 * We then specify what transport method we would like to use, websocket in our case, and if
-                 * the connection should try to upgrade to a better transport method if possible, which we put
-                 * as false.
-                 */
-                this.socketTimer = io(environment.serverUrl + type, { transports: ['websocket'], upgrade: false });
-                break;
-            case Gateways.Chat:
-                this.socketChat = io(environment.serverUrl + type, { transports: ['websocket'], upgrade: false });
-                break;
-        }
+        this.setSocket(type, io(environment.serverUrl + type, { transports: ['websocket'], upgrade: false }));
     }
 
     /**
      * Disconnects the socket of a give gateway.
      *
-     * @param type the gateway to disconnect from.
+     * @param type The gateway to disconnect from.
      */
     disconnect(type: Gateways) {
-        switch (type) {
-            case Gateways.Timer:
-                this.socketTimer.disconnect();
-                break;
-            case Gateways.Chat:
-                this.socketChat.disconnect();
-                break;
-        }
+        return this.getSocket(type).disconnect();
     }
 
     /**
@@ -90,44 +102,22 @@ export class SocketHandler {
      * Associates a given event with an action and a gateway and executes said action on even for the
      * given gateway.
      *
-     * @param event the event to process
-     * @param action the action to perform on that event
-     * @param type the socket on which this should all be performed.
+     * @param type The socket on which this should all be performed.
+     * @param event The event to process
+     * @param action The action to perform on that event
      */
-    on<T>(event: string, action: (data: T) => void, type: Gateways): void {
-        switch (type) {
-            case Gateways.Timer:
-                this.socketTimer.on(event, action);
-                break;
-            case Gateways.Chat:
-                this.socketChat.on(event, action);
-                break;
-        }
+    on<T>(type: Gateways, event: string, action: (data: T) => void): void {
+        this.getSocket(type).on(event, action);
     }
 
     /**
      * The method emits an event on a given gateway and the data linked to that event if provided.
      *
-     * @param type the gateway on which to send the event
-     * @param event the event to emit via the socket.
-     * @param data the data provided with the event. This parameter is options and may not be provided.
+     * @param type The gateway on which to send the event
+     * @param event The event to emit via the socket.
+     * @param data The data provided with the event. This parameter is options and may not be provided.
      */
     send<T>(type: Gateways, event: string, data?: T): void {
-        switch (type) {
-            case Gateways.Timer:
-                if (data) {
-                    this.socketTimer.emit(event, data);
-                } else {
-                    this.socketTimer.emit(event);
-                }
-                break;
-            case Gateways.Chat:
-                if (data) {
-                    this.socketChat.emit(event, data);
-                } else {
-                    this.socketChat.emit(event);
-                }
-                break;
-        }
+        this.getSocket(type).emit(event, data); // Do we need to add data as optional?
     }
 }
