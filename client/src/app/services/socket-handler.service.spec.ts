171fd8e848f626872834519a39dcf314985b1e81
Galen Hu <galenhu@hotmail.com>
2023-03-02 19:53:07 -0500

    removed fdescribe... is it perhaps a typo?

--- client/src/app/services/socket-handler.service.spec.ts
+++ client/src/app/services/socket-handler.service.spec.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 
 import { Gateways, SocketHandler } from './socket-handler.service';
 
-fdescribe('SocketClientService', () => {
+describe('SocketClientService', () => {
     let service: SocketHandler;
 
     beforeEach(() => {
