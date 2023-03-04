6514832d01213ad8a3c5b50c17687855d488823c
A123wq123 <charles.degrandpre@polymtl.ca>
2023-03-03 20:53:42 -0500

    feature/Codereview/removed constants created in test file of drawService into the Constants file

--- client/src/app/services/drawService/draw.service.spec.ts
+++ client/src/app/services/drawService/draw.service.spec.ts
@@ -2,18 +2,16 @@ import { TestBed } from '@angular/core/testing';
 import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
 import { Vec2 } from '@app/interfaces/vec2';
 import { DrawService } from '@app/services/drawService/draw.service';
+import { Constants } from '@common/constants';
 
 describe('DrawService', () => {
     let service: DrawService;
     let ctxStub: CanvasRenderingContext2D;
 
-    const CANVAS_WIDTH = 640;
-    const CANVAS_HEIGHT = 480;
-
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(DrawService);
-        ctxStub = CanvasTestHelper.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT).getContext('2d', {
+        ctxStub = CanvasTestHelper.createCanvas(Constants.DEFAULT_WIDTH, Constants.DEFAULT_HEIGHT).getContext('2d', {
             willReadFrequently: true,
         }) as CanvasRenderingContext2D;
         service.context = ctxStub;
@@ -24,11 +22,11 @@ describe('DrawService', () => {
     });
 
     it('width should return the width of the grid canvas', () => {
-        expect(service.width).toEqual(CANVAS_WIDTH);
+        expect(service.width).toEqual(Constants.DEFAULT_WIDTH);
     });
 
     it('height should return the height of the grid canvas', () => {
-        expect(service.height).toEqual(CANVAS_HEIGHT);
+        expect(service.height).toEqual(Constants.DEFAULT_HEIGHT);
     });
 
     it('drawError should have red text', () => {
