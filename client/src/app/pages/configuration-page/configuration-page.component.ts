5adb0044c04ff05a2817d94e496610d207f3f30a
Primexus <pierre.tran@ymail.com>
2023-03-10 13:39:42 -0500

    Moved header to top.

--- client/src/app/pages/configuration-page/configuration-page.component.ts
+++ client/src/app/pages/configuration-page/configuration-page.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 import { LevelService } from '@app/services/levelService/level.service';
 
-@Component({
-    selector: 'app-configuration-page',
-    templateUrl: './configuration-page.component.html',
-    styleUrls: ['./configuration-page.component.scss'],
-})
 /**
  * This component represents the page where the user can modify, delete or create new games. It is accessible from
  * a button inside the main page component and can redirect to the creation page.
@@ -13,6 +8,11 @@ import { LevelService } from '@app/services/levelService/level.service';
  * @author Louis Félix St-Amour, Galen Hu & Simon Gagné
  * @class Configuration Component
  */
+@Component({
+    selector: 'app-configuration-page',
+    templateUrl: './configuration-page.component.html',
+    styleUrls: ['./configuration-page.component.scss'],
+})
 export class ConfigurationPageComponent {
     constructor(public levelService: LevelService) {}
 }
