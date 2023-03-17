d7169dc80be87c84c8786e7312f78efbaa4e1206
JunaidJunaidJunaidJunaid <97924533+JunaidJunaidJunaidJunaid@users.noreply.github.com>
2023-03-17 14:57:47 -0400

    Setup test bed for game gateway, implement a few tests for on click

--- server/app/gateways/game/game.gateway.spec.ts
+++ server/app/gateways/game/game.gateway.spec.ts
@@ -1,58 +1,121 @@
+import { GameData, GameService, GameState } from '@app/services/game/game.service';
 import { ImageService } from '@app/services/image/image.service';
-import { IoAdapter } from '@nestjs/platform-socket.io';
+import { TimerService } from '@app/services/timer/timer.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { createStubInstance, SinonStubbedInstance } from 'sinon';
-import { BroadcastOperator, Namespace, Server, Socket } from 'socket.io';
+import { Namespace, Server, Socket } from 'socket.io';
 import { GameGateway } from './game.gateway';
 
 describe('GameGateway', () => {
     let gateway: GameGateway;
     let socket: SinonStubbedInstance<Socket>;
-    let secondSocket: SinonStubbedInstance<Socket>;
+    let socketSecondPlayer: SinonStubbedInstance<Socket>;
     let server: SinonStubbedInstance<Server>;
+    let gameService: SinonStubbedInstance<GameService>;
+    let timerService: SinonStubbedInstance<TimerService>;
+    let gameState: GameState;
+
+    const gameData: GameData = {
+        differencePixels: [],
+        totalDifferences: 0,
+        amountOfDifferencesFound: 0,
+    };
 
     beforeEach(async () => {
-        const broadcastMock = {} as BroadcastOperator<unknown, unknown>;
-        broadcastMock.emit = jest.fn();
-        broadcastMock.to = jest.fn();
-
-        const roomMap = new Map();
-        roomMap.set(0, { size: 1 });
-
+        gameState = {
+            gameId: 1,
+            foundDifferences: [],
+            playerName: 'Alice',
+        };
+        timerService = createStubInstance<TimerService>(TimerService);
+        gameService = createStubInstance<GameService>(GameService);
         socket = createStubInstance<Socket>(Socket);
+        socketSecondPlayer = createStubInstance<Socket>(Socket);
         server = createStubInstance<Server>(Server);
-        secondSocket = createStubInstance<Socket>(Socket);
-
-        Object.defineProperty(socket, 'id', { value: '0' });
-        Object.defineProperty(secondSocket, 'id', { value: '1' });
-        Object.defineProperty(server, 'sockets', { value: {} as Namespace });
-        Object.defineProperty(server.sockets, 'adapter', { value: {} as IoAdapter });
-        Object.defineProperty(socket, 'broadcast', { value: broadcastMock });
-        Object.defineProperty(server.sockets.adapter, 'rooms', { value: roomMap });
-
-        jest.spyOn(server, 'to').mockReturnValue(broadcastMock);
-        jest.spyOn(socket.broadcast, 'to').mockReturnValue(broadcastMock);
-        jest.spyOn(global.Math, 'random').mockReturnValue(0);
 
         const module: TestingModule = await Test.createTestingModule({
-            providers: [GameGateway, ImageService],
+            providers: [
+                GameGateway,
+                ImageService,
+                { provide: GameService, useValue: gameService },
+                { provide: TimerService, useValue: timerService },
+                { provide: Socket, useValue: socket },
+                { provide: Server, useValue: server },
+            ],
         }).compile();
 
         gateway = module.get<GameGateway>(GameGateway);
+        const socketNameSpace = {} as Namespace;
+        const socketMaps = new Map<string, Socket>();
+        socketMaps.set('1', socketSecondPlayer);
+        Object.defineProperty(server, 'sockets', { value: socketNameSpace });
+        Object.defineProperty(socketNameSpace, 'sockets', { value: socketMaps });
         gateway['server'] = server;
-    });
-
-    afterEach(() => {
-        jest.clearAllTimers();
-        jest.clearAllMocks();
-        gateway['playerRoomMap'].clear();
-        gateway['playerGameMap'].clear();
-        for (const interval of gateway['timeIntervalMap'].values()) {
-            clearInterval(interval);
-        }
     });
 
     it('should be defined', () => {
         expect(gateway).toBeDefined();
+    });
+
+    it('should call timer service and game service when player joins a solo game', () => {
+        const timerSpy = jest.spyOn(timerService, 'startTimer');
+        const gameSpy = jest.spyOn(gameService, 'createNewGame');
+        const data = { levelId: 1, playerName: 'test' };
+        gateway.onJoinSoloClassicGame(socket, data);
+        expect(gameSpy).toHaveBeenCalled();
+        expect(timerSpy).toHaveBeenCalled();
+    });
+
+    it('should emit back to socket when player clicks', async () => {
+        jest.spyOn(gameService, 'getImageInfoOnClick').mockReturnValue(Promise.resolve(gameData));
+        jest.spyOn(gameService, 'verifyWinCondition').mockReturnValue(false);
+        jest.spyOn(gameService, 'getGameState').mockReturnValue(gameState);
+        const emitSpy = jest.spyOn(socket, 'emit');
+        await gateway.onClick(socket, 1);
+        expect(emitSpy).toBeCalledWith('onProcessedClick', gameData);
+    });
+
+    it('should emit to the opponent when player clicks', async () => {
+        gameState.secondPlayerId = '1';
+        jest.spyOn(gameService, 'getImageInfoOnClick').mockReturnValue(Promise.resolve(gameData));
+        jest.spyOn(gameService, 'verifyWinCondition').mockReturnValue(false);
+        jest.spyOn(gameService, 'getGameState').mockReturnValue(gameState);
+        const emitSpy = jest.spyOn(socket, 'emit');
+        const emitSecondPlayerSpy = jest.spyOn(socketSecondPlayer, 'emit');
+        await gateway.onClick(socket, 1);
+        expect(emitSpy).toBeCalledWith('onProcessedClick', gameData);
+        expect(emitSecondPlayerSpy).toBeCalledWith('onProcessedClick', gameData);
+    });
+
+    it('should emit a victory event if player wins', async () => {
+        jest.spyOn(gameService, 'getImageInfoOnClick').mockReturnValue(Promise.resolve(gameData));
+        jest.spyOn(gameService, 'verifyWinCondition').mockReturnValue(true);
+        jest.spyOn(gameService, 'getGameState').mockReturnValue(gameState);
+
+        const timerSpy = jest.spyOn(timerService, 'stopTimer');
+        const gameSpy = jest.spyOn(gameService, 'deleteUserFromGame');
+        const emitSpy = jest.spyOn(socket, 'emit');
+
+        await gateway.onClick(socket, 1);
+
+        expect(emitSpy).toBeCalledWith('onProcessedClick', gameData);
+        expect(emitSpy).toBeCalledWith('onVictory');
+        expect(timerSpy).toBeCalled();
+        expect(gameSpy).toBeCalled();
+    });
+
+    it('should emit a defeat to the opponent event if player wins', async () => {
+        gameState.secondPlayerId = '1';
+        jest.spyOn(gameService, 'getImageInfoOnClick').mockReturnValue(Promise.resolve(gameData));
+        jest.spyOn(gameService, 'verifyWinCondition').mockReturnValue(true);
+        jest.spyOn(gameService, 'getGameState').mockReturnValue(gameState);
+        jest.spyOn(timerService, 'stopTimer');
+        jest.spyOn(gameService, 'deleteUserFromGame');
+        const emitSecondPlayerSpy = jest.spyOn(socketSecondPlayer, 'emit');
+
+        await gateway.onClick(socket, 1);
+
+        expect(emitSecondPlayerSpy).toBeCalledWith('onProcessedClick', gameData);
+        expect(emitSecondPlayerSpy).toBeCalledWith('onDefeat');
     });
 });
