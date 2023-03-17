e796fe439668cb4fe65ba1c9eb55ff96a9261a54
JunaidJunaidJunaidJunaid <97924533+JunaidJunaidJunaidJunaid@users.noreply.github.com>
2023-03-17 11:23:51 -0400

    Started making tests for selection page service

--- client/src/app/services/selectionPageService/selection-page.service.spec.ts
+++ client/src/app/services/selectionPageService/selection-page.service.spec.ts
@@ -1,23 +1,89 @@
 import { TestBed } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Level } from '@app/levels';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { LevelService } from '@app/services/levelService/level.service';
 import { PopUpService } from '@app/services/popUpService/pop-up.service';
 import { SocketHandler } from '@app/services/socket-handler.service';
-import { SelectionPageService } from './selection-page.service';
+import { of } from 'rxjs';
+import { SelectionData, SelectionPageService } from './selection-page.service';
 
 describe('SelectionPageService', () => {
     let service: SelectionPageService;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let popUpServiceSpy: any;
+    let socketHandlerSpy: jasmine.SpyObj<SocketHandler>;
+    let levelServiceSpy: jasmine.SpyObj<LevelService>;
+    let dialogRefSpy: jasmine.SpyObj<MatDialogRef<unknown>>;
 
     beforeEach(() => {
+        socketHandlerSpy = jasmine.createSpyObj('SocketHandler', ['on', 'isSocketAlive', 'send', 'connect', 'removeListener']);
+        levelServiceSpy = jasmine.createSpyObj('LevelService', ['']);
+        dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['afterClosed', 'close']);
+        dialogRefSpy.afterClosed.and.returnValue(of(true));
+
+        popUpServiceSpy = {
+            openDialog: jasmine.createSpy('openDialog').and.returnValue(of(undefined)),
+            dialogRef: dialogRefSpy,
+        };
+
         TestBed.configureTestingModule({
             imports: [AppRoutingModule, AppMaterialModule],
-            providers: [LevelService, SocketHandler, PopUpService],
+            providers: [
+                { provide: LevelService, useValue: levelServiceSpy },
+                { provide: PopUpService, useValue: popUpServiceSpy },
+                { provide: SocketHandler, useValue: socketHandlerSpy },
+                { provide: MatDialogRef, useValue: dialogRefSpy },
+            ],
         });
         service = TestBed.inject(SelectionPageService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should open a dialog when a player creates a game and when the player closes the dialog', () => {
+        const waitForMatchSpy = spyOn(service, 'waitForMatch' as never);
+        service.startGameDialog(1);
+        expect(popUpServiceSpy.openDialog).toHaveBeenCalled();
+        expect(waitForMatchSpy).toHaveBeenCalled();
+    });
+
+    it('should emit a socket event when the user waits for a player and when player closes the dialog', () => {
+        const id = 1;
+        const name = 'Alice';
+        service['waitForMatch'](id, name);
+        expect(popUpServiceSpy.openDialog).toHaveBeenCalled();
+        expect(socketHandlerSpy.send).toHaveBeenCalledWith('game', 'onGameSelection', { levelId: id, playerName: name });
+        expect(socketHandlerSpy.send).toHaveBeenCalledWith('game', 'onGameCancelledWhileWaitingForSecondPlayer', {});
+    });
+
+    it('should update levels correctly by updating the canJoin attribute', () => {
+        const data: SelectionData = {
+            levelId: 1,
+            canJoin: true,
+        };
+        const levels: Level[] = [
+            {
+                id: 1,
+                name: 'Level 1',
+                playerSolo: [],
+                timeSolo: [],
+                playerMulti: [],
+                timeMulti: [],
+                isEasy: true,
+                nbDifferences: 1,
+                canJoin: false,
+            },
+        ];
+        spyOnProperty(levelServiceSpy, 'shownLevels' as never).and.returnValue(levels);
+        service['updateSelection'](data, levelServiceSpy);
+    });
+
+    it('should open a dialog when the player enters an invalid name', () => {
+        service['openInvalidNameDialog']();
+        expect(popUpServiceSpy.openDialog).toHaveBeenCalled();
     });
 });
--- client/src/app/services/selectionPageService/selection-page.service.ts
+++ client/src/app/services/selectionPageService/selection-page.service.ts
@@ -4,17 +4,24 @@ import { LevelService } from '@app/services/levelService/level.service';
 import { DialogData, PopUpService } from '@app/services/popUpService/pop-up.service';
 import { SocketHandler } from '@app/services/socket-handler.service';
 
-interface SelectionData {
+export interface SelectionData {
     levelId: number;
     canJoin: boolean;
 }
 
-interface StartGameData {
+export interface StartGameData {
     levelId: number;
     playerName: string;
     secondPlayerName: string;
 }
 
+/**
+ * This service is used to handle the selection page.
+ * It handles the socket events and the popups.
+ *
+ * @author Junaid Qureshi
+ * @class SelectionPageService
+ */
 @Injectable({
     providedIn: 'root',
 })
@@ -34,88 +41,45 @@ export class SelectionPageService implements OnDestroy {
         this.socketHandler.removeListener('rejectedGame');
     }
 
+    /**
+     * This method is used to setup all the socket listeners.
+     * It handles all the popups that are displayed when a player creates/joins a game.
+     *
+     * @param levelService The level service used to update the level cards.
+     */
     setupSocket(levelService: LevelService): void {
         if (!this.socketHandler.isSocketAlive('game')) {
             this.socketHandler.connect('game');
-            this.socketHandler.on('game', 'updateSelection', (data) => {
-                const selectionData: SelectionData = data as SelectionData;
-                levelService.levelsToShow.forEach((level) => {
-                    if (level.id === selectionData.levelId) {
-                        level.canJoin = selectionData.canJoin;
-                    }
-                });
-            });
-            this.socketHandler.on('game', 'invalidName', () => {
-                this.popUpService.dialogRef.close();
-                const invalidNameDialogData: DialogData = {
-                    textToSend: 'Le nom choisi est trop court, veuillez en choisir un autre',
-                    closeButtonMessage: 'OK',
-                };
-                this.popUpService.openDialog(invalidNameDialogData);
-            });
-            this.socketHandler.on('game', 'toBeAccepted', () => {
-                this.waitingForSecondPlayer = false;
-                this.popUpService.dialogRef.close();
-                const toBeAcceptedDialogData: DialogData = {
-                    textToSend: "Partie trouvée ! En attente de l'approbation de l'autre joueur.",
-                    closeButtonMessage: 'Annuler',
-                };
-                this.popUpService.openDialog(toBeAcceptedDialogData);
-                this.popUpService.dialogRef.afterClosed().subscribe(() => {
-                    if (this.waitingForAcceptation) {
-                        this.socketHandler.send('game', 'onGameRejected', {});
-                    }
-                });
-            });
-            this.socketHandler.on('game', 'playerSelection', (name) => {
-                this.waitingForSecondPlayer = false;
-                this.popUpService.dialogRef.close();
-                const toBeAcceptedDialogData: DialogData = {
-                    textToSend: 'Voulez-vous autoriser ' + name + ' à participer à votre jeu ?',
-                    closeButtonMessage: 'Annuler',
-                    isConfirmation: true,
-                };
-                this.popUpService.openDialog(toBeAcceptedDialogData);
-                this.popUpService.dialogRef.afterClosed().subscribe((confirmation) => {
-                    if (confirmation) {
-                        this.socketHandler.send('game', 'onGameAccepted', {});
-                    } else if (this.waitingForAcceptation) {
-                        this.socketHandler.send('game', 'onGameRejected', {});
-                    }
-                });
-            });
-            this.socketHandler.on('game', 'startClassicMultiplayerGame', (data) => {
-                const startGameData: StartGameData = data as StartGameData;
-                this.waitingForAcceptation = false;
-                this.popUpService.dialogRef.close();
-                this.router.navigate([`/game/${startGameData.levelId}/`], {
-                    queryParams: { playerName: startGameData.playerName, opponent: startGameData.secondPlayerName },
-                });
-            });
-
-            this.socketHandler.on('game', 'rejectedGame', () => {
-                console.log('rejected');
-                this.waitingForAcceptation = false;
-                this.popUpService.dialogRef.close();
-            });
         }
-    }
-
-    waitForMatch(id: number, result: string): void {
-        this.socketHandler.send('game', 'onGameSelection', { levelId: id, playerName: result });
-        const loadingDialogData: DialogData = {
-            textToSend: "En attente d'un autre joueur",
-            closeButtonMessage: 'Annuler',
-        };
-        this.popUpService.openDialog(loadingDialogData);
-        this.popUpService.dialogRef.afterClosed().subscribe(() => {
-            if (this.waitingForSecondPlayer) {
-                this.socketHandler.send('game', 'onGameCancelledWhileWaitingForSecondPlayer', {});
-            }
+        this.socketHandler.on('game', 'updateSelection', (data) => {
+            this.updateSelection(data as SelectionData, levelService);
+        });
+        this.socketHandler.on('game', 'invalidName', () => {
+            this.openInvalidNameDialog();
+        });
+        this.socketHandler.on('game', 'toBeAccepted', () => {
+            this.openToBeAcceptedDialog();
+        });
+        this.socketHandler.on('game', 'playerSelection', (name) => {
+            this.openPlayerSelectionDialog(name as string);
+        });
+        this.socketHandler.on('game', 'startClassicMultiplayerGame', (data) => {
+            this.startMultiplayerGame(data as StartGameData);
+        });
+        this.socketHandler.on('game', 'rejectedGame', () => {
+            this.waitingForAcceptation = false;
+            this.popUpService.dialogRef.close();
         });
     }
 
-    // TODO: Rework this function
+    // TODO: Rework this function -Pierre
+
+    /**
+     * This method is called when a player creates a game.
+     * It asks the player for his name.
+     *
+     * @param levelId The id of the level that the player wants to play.
+     */
     startGameDialog(levelId: number): void {
         this.waitingForAcceptation = true;
         this.waitingForSecondPlayer = true;
@@ -134,10 +98,115 @@ export class SelectionPageService implements OnDestroy {
         };
         this.popUpService.openDialog(saveDialogData);
         this.popUpService.dialogRef.afterClosed().subscribe((result) => {
+            console.log(result);
             if (result) {
-                this.waitingForSecondPlayer = true;
                 this.waitForMatch(levelId, result);
             }
+        });
+    }
+
+    /**
+     * This method is called when a player creates a game.
+     * It tells the player to wait for the other player to join the game.
+     *
+     * @param id The id of the level that the player wants to play.
+     * @param name The name of the player.
+     */
+    private waitForMatch(id: number, name: string): void {
+        this.socketHandler.send('game', 'onGameSelection', { levelId: id, playerName: name });
+        const loadingDialogData: DialogData = {
+            textToSend: "En attente d'un autre joueur",
+            closeButtonMessage: 'Annuler',
+        };
+        this.popUpService.openDialog(loadingDialogData);
+        this.popUpService.dialogRef.afterClosed().subscribe(() => {
+            if (this.waitingForSecondPlayer) {
+                this.socketHandler.send('game', 'onGameCancelledWhileWaitingForSecondPlayer', {});
+            }
+        });
+    }
+
+    /**
+     * This methods is called when a game is created in the server.
+     * It updates the cards component to show the game as joinable.
+     *
+     * @param data The data received from the server used to update the level cards.
+     * @param levelService The level service used to update the level cards.
+     */
+    private updateSelection(data: SelectionData, levelService: LevelService): void {
+        levelService.levelsToShow.forEach((level) => {
+            if (level.id === data.levelId) {
+                level.canJoin = data.canJoin;
+            }
+        });
+    }
+
+    /**
+     * This method is called if the player name is invalid.
+     */
+    private openInvalidNameDialog(): void {
+        this.popUpService.dialogRef.close();
+        const invalidNameDialogData: DialogData = {
+            textToSend: 'Le nom choisi est trop court, veuillez en choisir un autre',
+            closeButtonMessage: 'OK',
+        };
+        this.popUpService.openDialog(invalidNameDialogData);
+    }
+
+    /**
+     * This method is called when a match has been found and the user is waiting to be accepted.
+     * It opens a dialog to inform the user that he is waiting for the other player to accept.
+     */
+    private openToBeAcceptedDialog(): void {
+        this.waitingForSecondPlayer = false;
+        this.popUpService.dialogRef.close();
+        const toBeAcceptedDialogData: DialogData = {
+            textToSend: "Partie trouvée ! En attente de l'approbation de l'autre joueur.",
+            closeButtonMessage: 'Annuler',
+        };
+        this.popUpService.openDialog(toBeAcceptedDialogData);
+        this.popUpService.dialogRef.afterClosed().subscribe(() => {
+            if (this.waitingForAcceptation) {
+                this.socketHandler.send('game', 'onGameRejected', {});
+            }
+        });
+    }
+
+    /**
+     * This method is called when a player wants to join a game.
+     * It opens a dialog to ask the user if he wants to accept the player.
+     *
+     * @param name The name of the player that wants to join the game
+     */
+    private openPlayerSelectionDialog(name: string): void {
+        this.waitingForSecondPlayer = false;
+        this.popUpService.dialogRef.close();
+        const toBeAcceptedDialogData: DialogData = {
+            textToSend: 'Voulez-vous autoriser ' + name + ' à participer à votre jeu ?',
+            closeButtonMessage: 'Annuler',
+            isConfirmation: true,
+        };
+        this.popUpService.openDialog(toBeAcceptedDialogData);
+        this.popUpService.dialogRef.afterClosed().subscribe((confirmation) => {
+            if (confirmation) {
+                this.socketHandler.send('game', 'onGameAccepted', {});
+            } else if (this.waitingForAcceptation) {
+                this.socketHandler.send('game', 'onGameRejected', {});
+            }
+        });
+    }
+
+    /**
+     * This method is called when a game is started in the server.
+     * It redirects the user to the game page.
+     *
+     * @param data The data received from the server used to start the game.
+     */
+    private startMultiplayerGame(data: StartGameData): void {
+        this.waitingForAcceptation = false;
+        this.popUpService.dialogRef.close();
+        this.router.navigate([`/game/${data.levelId}/`], {
+            queryParams: { playerName: data.playerName, opponent: data.secondPlayerName },
         });
     }
 }
