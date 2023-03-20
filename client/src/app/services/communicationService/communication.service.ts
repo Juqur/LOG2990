dfa3fc3c4a18b025b33f6ecd25e41daa51100fad
Primexus <pierre.tran@ymail.com>
2023-03-20 15:08:22 -0400

    Removed another console.log

--- client/src/app/services/communicationService/communication.service.ts
+++ client/src/app/services/communicationService/communication.service.ts
@@ -37,7 +37,6 @@ export class CommunicationService {
      * @returns an observable on the appropriate level.
      */
     getLevel(levelId: number): Observable<Level> {
-        console.log('wtf');
         return this.http.get<Level>(`${this.baseUrl}api` + '/image/' + levelId).pipe(catchError(this.handleError<Level>('basicGet')));
     }
 
