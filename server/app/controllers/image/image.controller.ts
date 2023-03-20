56719db78db8ca4a76aea21355e89487cb81e684
JunaidJunaidJunaidJunaid <97924533+JunaidJunaidJunaidJunaid@users.noreply.github.com>
2023-03-20 18:00:10 -0400

    Added proper HTTP codes to api

--- server/app/controllers/image/image.controller.ts
+++ server/app/controllers/image/image.controller.ts
@@ -1,10 +1,15 @@
 import { Message } from '@app/model/schema/message.schema';
 import { GameService } from '@app/services/game/game.service';
 import { ImageService } from '@app/services/image/image.service';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { Level } from 'assets/data/level';
 import { FileSystemStoredFile, FormDataRequest } from 'nestjs-form-data';
+
+enum HttpCodes {
+    OK = 200,
+    CREATED = 201,
+}
 
 /**
  * This controller provides the server API requests for the image data.
@@ -24,6 +29,7 @@ export class ImageController {
      * @returns The array of levels data stored in the server.
      */
     @Get('/allLevels')
+    @HttpCode(HttpCodes.OK)
     @ApiOkResponse({
         description: 'Returns data for all levels',
     })
@@ -58,6 +64,7 @@ export class ImageController {
         description: 'Returns data for a level',
     })
     @Get('/:id')
+    @HttpCode(HttpCodes.OK)
     async getLevel(@Param('id') id: string): Promise<Level> {
         return await this.imageService.getLevel(parseInt(id, 10));
     }
@@ -69,6 +76,7 @@ export class ImageController {
      * @returns The number of differences between the two images.
      */
     @Get('/differenceCount')
+    @HttpCode(HttpCodes.OK)
     @ApiOkResponse({
         description: 'Returns the number of differences between the two images',
     })
@@ -86,6 +94,7 @@ export class ImageController {
         description: 'Writes the level data onto a json file for the game information and the images into the assets folder.',
     })
     @Post('/postLevel')
+    @HttpCode(HttpCodes.CREATED)
     @FormDataRequest({ storage: FileSystemStoredFile, autoDeleteFile: false, fileSystemStoragePath: '../server/assets/images' })
     async writeLevelData(@Body() formData: unknown): Promise<Message> {
         return await this.imageService.writeLevelData(formData);
