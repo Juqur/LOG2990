f28955db864d80130c0615968a8947cb6f26de5f
Primexus <pierre.tran@ymail.com>
2023-03-20 16:17:43 -0400

    Fixed MouseService documentation.

--- client/src/app/services/mouseService/mouse.service.ts
+++ client/src/app/services/mouseService/mouse.service.ts
@@ -2,16 +2,15 @@ import { Injectable } from '@angular/core';
 import { Vec2 } from '@app/interfaces/vec2';
 import { Constants, MouseButton } from '@common/constants';
 
-@Injectable({
-    providedIn: 'root',
-})
-
 /**
  * This service is used to get the position of the mouse in the canvas.
  *
  * @author Junaid Qureshi
  * @class MouseService
  */
+@Injectable({
+    providedIn: 'root',
+})
 export class MouseService {
     private mousePosition: Vec2 = { x: 0, y: 0 };
     private canClick: boolean = true;
@@ -20,10 +19,10 @@ export class MouseService {
      * Takes the mouse event to calculate the position of the mouse
      * and returns the absolute position of the mouse in the canvas.
      *
-     * @param event the mouse event
-     * @returns the position of the mouse in the canvas
+     * @param event The mouse event.
+     * @returns The position of the mouse in the canvas.
      */
-    getMousePosition(event: MouseEvent) {
+    getMousePosition(event: MouseEvent): number | null {
         if (event.button === MouseButton.Left && this.canClick) {
             this.mousePosition = { x: event.offsetX, y: event.offsetY };
             return this.mousePosition.x * Constants.PIXEL_SIZE + this.mousePosition.y * Constants.DEFAULT_WIDTH * Constants.PIXEL_SIZE;
@@ -34,7 +33,7 @@ export class MouseService {
     /**
      * Returns the x coordinate of the last loaded click.
      *
-     * @returns the value of the x coordinate.
+     * @returns The value of the x coordinate.
      */
     getX(): number {
         return this.mousePosition.x;
@@ -59,9 +58,9 @@ export class MouseService {
     }
 
     /**
-     * Returns the boolean indicating if a click can be processed.
+     * Getter of the canClick attribute.
      *
-     * @returns the canClick boolean.
+     * @returns The boolean indicating if a click can be processed.
      */
     getCanClick(): boolean {
         return this.canClick;
