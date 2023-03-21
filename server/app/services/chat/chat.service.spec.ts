1578596d0b1bca80dffc36d168c612071cc00fb7
Primexus <pierre.tran@ymail.com>
2023-03-21 11:17:21 -0400

    Implemented tests for chat service.

--- server/app/services/chat/chat.service.spec.ts
+++ server/app/services/chat/chat.service.spec.ts
@@ -1,16 +1,22 @@
+import { GameState } from '@app/services/game/game.service';
+import { ChatMessage } from '@common/chat-messages';
+import { GameData } from '@common/game-data';
 import { Test, TestingModule } from '@nestjs/testing';
 import { createStubInstance, SinonStubbedInstance } from 'sinon';
 import { Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 
 describe('ChatService', () => {
+    const gameState = {} as unknown as GameState;
     let service: ChatService;
     let socket: SinonStubbedInstance<Socket>;
-    // let emitSpy: jest.SpyInstance;
+    let emitSpy: jest.SpyInstance;
+    let emitToSpy: jest.SpyInstance;
 
     beforeEach(async () => {
         socket = createStubInstance<Socket>(Socket);
-        // emitSpy = jest.spyOn(socket, 'to').mockImplementation(() => ({ emit: jest.fn() } as never));
+        emitSpy = jest.spyOn(socket, 'emit');
+        emitToSpy = jest.spyOn(socket, 'to').mockImplementation(() => ({ emit: jest.fn() } as never));
 
         const module: TestingModule = await Test.createTestingModule({
             providers: [ChatService],
@@ -24,8 +30,74 @@ describe('ChatService', () => {
     });
 
     describe('sendSystemMessage', () => {
-        it('should call getGameState', () => {
-            // TODO
+        let data: GameData;
+        let getSystemChatMessageSpy: jest.SpyInstance;
+
+        beforeEach(() => {
+            data = {
+                differencePixels: [0, 1],
+            } as unknown as GameData;
+            getSystemChatMessageSpy = jest.spyOn(service, 'getSystemChatMessage' as never).mockImplementation();
+        });
+
+        it('should call emit', () => {
+            service.sendSystemMessage(socket, data, gameState);
+            expect(emitSpy).toHaveBeenCalledTimes(1);
+        });
+
+        it('should getSystemChatMessageSpy', () => {
+            service.sendSystemMessage(socket, data, gameState);
+            expect(getSystemChatMessageSpy).toHaveBeenCalledTimes(1);
+        });
+
+        it('should call emit to the other socket if the other socket exists', () => {
+            gameState.otherSocketId = 'otherSocketId';
+            service.sendSystemMessage(socket, data, gameState);
+            expect(emitToSpy).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('sendToBothPlayers', () => {
+        const message = {} as unknown as ChatMessage;
+
+        it('should call emit', () => {
+            service.sendToBothPlayers(socket, message, gameState);
+            expect(emitSpy).toHaveBeenCalledTimes(1);
+        });
+
+        it('should call emit to the other socket', () => {
+            service.sendToBothPlayers(socket, message, gameState);
+            expect(emitToSpy).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('abandonMessage', () => {
+        let getSystemChatMessageSpy: jest.SpyInstance;
+
+        beforeEach(() => {
+            getSystemChatMessageSpy = jest.spyOn(service, 'getSystemChatMessage' as never).mockImplementation();
+        });
+
+        it('should call emit to the other socket', () => {
+            service.abandonMessage(socket, gameState);
+            expect(emitToSpy).toHaveBeenCalledTimes(1);
+        });
+
+        it('should call getSystemChatMessage', () => {
+            service.abandonMessage(socket, gameState);
+            expect(getSystemChatMessageSpy).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('getSystemChatMessage', () => {
+        it('should send the correct message form', () => {
+            const expectedMessage = 'This should be the message';
+            const result = service['getSystemChatMessage'](expectedMessage);
+            expect(result).toStrictEqual({
+                sender: 'Système',
+                senderId: 'system',
+                text: expectedMessage,
+            });
         });
     });
 });
