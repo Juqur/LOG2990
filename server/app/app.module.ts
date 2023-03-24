8c9ed62f16ad2596ca04dacd4b56e157e2a0af2c
LFSTA LAPTOP <algorix1217@gmail.com>
2023-03-24 10:25:24 -0400

    re-activated mongodb

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -1,7 +1,7 @@
 import { GameGateway } from '@app/gateways/game/game.gateway';
 import { Logger, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { NestjsFormDataModule } from 'nestjs-form-data';
 import { join } from 'path';
@@ -18,14 +18,14 @@ import { TimerService } from './services/timer/timer.service';
         ServeStaticModule.forRoot({
             rootPath: join('assets/images'),
         }),
-        // MongooseModule.forRootAsync({
-        //     imports: [ConfigModule],
-        //     inject: [ConfigService],
-        //     useFactory: async (config: ConfigService) => ({
-        //         uri: config.get<string>('DATABASE_CONNECTION_STRING'), // Loaded from .env
-        //     }),
-        // }),
-        // MongooseModule.forFeature([{ name: Course.name, schema: courseSchema }]),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                uri: config.get<string>('DATABASE_CONNECTION_STRING'), // Loaded from .env
+            }),
+        }),
+        MongooseModule.forFeature(),
     ],
     controllers: [ImageController],
     providers: [GameGateway, ImageService, GameService, ChatService, TimerService, Logger],
