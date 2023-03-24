c778f3a847f9bb4d8bf629312dbda497879490cf
JunaidJunaidJunaidJunaid <97924533+JunaidJunaidJunaidJunaid@users.noreply.github.com>
2023-03-24 15:50:03 -0400

    Added 100% coverage

--- client/src/app/services/main-page/main-page.service.spec.ts
+++ client/src/app/services/main-page/main-page.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
+import { Router } from '@angular/router';
+import { PopUpService } from '@app/services/pop-up/pop-up.service';
+import { SocketHandler } from '@app/services/socket-handler/socket-handler.service';
 import { MainPageService } from './main-page.service';
 
 describe('MainPageService', () => {
-  let service: MainPageService;
+    let service: MainPageService;
+    let socketHandlerSpy: jasmine.SpyObj<SocketHandler>;
+    let routerSpy: jasmine.SpyObj<Router>;
+    let popUpServiceSpy: jasmine.SpyObj<PopUpService>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MainPageService);
-  });
+    beforeEach(() => {
+        socketHandlerSpy = jasmine.createSpyObj('SocketHandler', ['isSocketAlive', 'connect']);
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+        popUpServiceSpy = jasmine.createSpyObj('PopUpService', ['openPopUp']);
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: SocketHandler, useValue: socketHandlerSpy },
+                { provide: Router, useValue: routerSpy },
+                { provide: PopUpService, useValue: popUpServiceSpy },
+            ],
+        });
+        service = TestBed.inject(MainPageService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
