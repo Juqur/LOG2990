a58fc25cfb69153ca1d4f67c5f5ef531c438adeb
Primexus <pierre.tran@ymail.com>
2023-03-30 18:45:44 -0400

    Fixed tests of socker-handler.

--- client/src/app/services/socket-handler/socket-handler.service.spec.ts
+++ client/src/app/services/socket-handler/socket-handler.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { io, Socket } from 'socket.io-client';
+import { Socket } from 'socket.io-client';
 import { SocketHandler } from './socket-handler.service';
 
-fdescribe('SocketClientService', () => {
+describe('SocketClientService', () => {
     let service: SocketHandler;
     let socketTest: Socket;
     const socketGateway1 = 'socket1';
@@ -60,110 +60,116 @@ fdescribe('SocketClientService', () => {
     });
 
     describe('connect', () => {
-        it('should connect to the given gateway', () => {
-            spyOn(io, 'call' as never);
-            // const spy = spyOn(service['sockets'], 'set');
+        it('should add the socket', () => {
+            const setSpy = spyOn(service['sockets'], 'set');
             service.connect(socketGateway1);
-            expect(spy).toHaveBeenCalledTimes(1);
+            expect(setSpy).toHaveBeenCalledWith(socketGateway1, jasmine.any(Socket));
         });
     });
 
-    // describe('disconnect', () => {
-    //     it('should disconnect', () => {
-    //         const spy = spyOn(socketTest, 'disconnect');
-    //         service.disconnect(socketGateway1);
-    //         expect(spy).toHaveBeenCalledTimes(1);
-    //     });
+    describe('disconnect', () => {
+        it('should disconnect', () => {
+            const spy = spyOn(socketTest, 'disconnect');
+            service.disconnect(socketGateway1);
+            expect(spy).toHaveBeenCalledTimes(1);
+        });
 
-    //     it('should not disconnect if socket is undefined', () => {
-    //         const spyDisconnect = spyOn(socketTest, 'disconnect');
-    //         spyOn(service, 'getSocket').and.returnValue(undefined);
-    //         service.disconnect(socketGateway1);
-    //         expect(spyDisconnect).not.toHaveBeenCalled();
-    //     });
-    // });
+        it('should not disconnect if socket is undefined', () => {
+            const spyDisconnect = spyOn(socketTest, 'disconnect');
+            spyOn(service, 'getSocket').and.returnValue(undefined);
+            service.disconnect(socketGateway1);
+            expect(spyDisconnect).not.toHaveBeenCalled();
+        });
+    });
 
-    // it('should disconnect all', () => {
-    //     const spy = spyOn(SocketTestHelper.prototype, 'disconnect');
-    //     service.disconnectAll();
-    //     expect(spy).toHaveBeenCalledTimes(2);
-    // });
+    describe('disconnectAll', () => {
+        it('should disconnect all', () => {
+            const spy = spyOn(socketTest, 'disconnect');
+            service.disconnectAll();
+            expect(spy).toHaveBeenCalledTimes(2);
+        });
+    });
 
-    // it('on should call socket.on with an event', () => {
-    //     const event = 'placeholder';
-    //     const action = jasmine.createSpy('action');
-    //     const spy = spyOn(socketTest, 'on');
-    //     service.on(socketGateway1, event, action);
-    //     expect(spy).toHaveBeenCalled();
-    //     expect(spy).toHaveBeenCalledWith(event, action);
-    // });
+    describe('on', () => {
+        const event = 'placeholder';
 
-    // it('on should not call socket.on if the socket is undefined', () => {
-    //     const event = 'placeholder';
-    //     const action = jasmine.createSpy('action');
-    //     const spy = spyOn(socketTest, 'on');
-    //     spyOn(service, 'getSocket').and.returnValue(undefined);
+        it('should call on with an event', () => {
+            const action = jasmine.createSpy('action');
+            const spy = spyOn(socketTest, 'on');
+            service.on(socketGateway1, event, action);
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(spy).toHaveBeenCalledWith(event, action);
+        });
 
-    //     service.on(socketGateway1, event, action);
-    //     expect(spy).not.toHaveBeenCalled();
-    // });
+        it('should not call on if the socket is undefined', () => {
+            const action = jasmine.createSpy('action');
+            const spy = spyOn(socketTest, 'on');
+            spyOn(service, 'getSocket').and.returnValue(undefined);
+            service.on(socketGateway1, event, action);
+            expect(spy).not.toHaveBeenCalled();
+        });
+    });
 
-    // it('send should call emit with data when using send', () => {
-    //     const event = 'placeholder';
-    //     const data = 0;
-    //     const spy = spyOn(socketTest, 'emit');
-    //     service.send(socketGateway1, event, data);
-    //     expect(spy).toHaveBeenCalled();
-    //     expect(spy).toHaveBeenCalledWith(event, data);
-    // });
+    describe('removeListener', () => {
+        it('should remove listener if listener exists', () => {
+            spyOn(service['socketListenersList'], 'indexOf').and.returnValue(0);
+            spyOn(service, 'getSocket').and.returnValue(socketTest);
+            const spySplice = spyOn(service['socketListenersList'], 'splice');
+            socketTest.off = jasmine.createSpy('off');
+            service.removeListener('game', 'placeholder');
+            expect(spySplice).toHaveBeenCalledTimes(1);
+            expect(socketTest.off).toHaveBeenCalledTimes(1);
+        });
 
-    // it('send should call emit without data when using send if data is undefined', () => {
-    //     const event = 'placeholder';
-    //     const data = undefined;
-    //     const spy = spyOn(socketTest, 'emit');
-    //     service.send(socketGateway1, event, data);
-    //     expect(spy).toHaveBeenCalled();
-    //     expect(spy).toHaveBeenCalledWith(event);
-    // });
+        it('should do nothing if listener does not exists', () => {
+            const index = -1;
+            spyOn(service['socketListenersList'], 'indexOf').and.returnValue(index);
+            spyOn(service, 'getSocket').and.returnValue(socketTest);
+            const spySplice = spyOn(service['socketListenersList'], 'splice');
+            socketTest.off = jasmine.createSpy('off');
+            service.removeListener('game', 'placeholder');
+            expect(spySplice).not.toHaveBeenCalled();
+            expect(socketTest.off).not.toHaveBeenCalled();
+        });
 
-    // it('send should not call emit if socket is undefined', () => {
-    //     const event = 'placeholder';
-    //     const data = 0;
-    //     const spy = spyOn(socketTest, 'emit');
-    //     spyOn(service, 'getSocket').and.returnValue(undefined);
+        it('should do nothing if socket is undefined', () => {
+            spyOn(service['socketListenersList'], 'indexOf').and.returnValue(0);
+            spyOn(service, 'getSocket').and.returnValue(undefined);
+            const spySplice = spyOn(service['socketListenersList'], 'splice');
+            socketTest.off = jasmine.createSpy('off');
+            service.removeListener('game', 'placeholder');
+            expect(spySplice).toHaveBeenCalledTimes(1);
+            expect(socketTest.off).not.toHaveBeenCalled();
+        });
+    });
 
-    //     service.send(socketGateway1, event);
-    //     service.send(socketGateway1, event, data);
-    //     expect(spy).not.toHaveBeenCalled();
-    // });
+    describe('send', () => {
+        const event = 'placeholder';
 
-    // it('should remove listener if listener exists', () => {
-    //     spyOn(service['socketListenersList'], 'indexOf').and.returnValue(0);
-    //     spyOn(service, 'getSocket').and.returnValue(socketTest);
-    //     const spySplice = spyOn(service['socketListenersList'], 'splice');
-    //     socketTest.off = jasmine.createSpy('off');
-    //     service.removeListener('game', 'placeholder');
-    //     expect(spySplice).toHaveBeenCalled();
-    //     expect(socketTest.off).toHaveBeenCalled();
-    // });
+        it('should call emit without data when using send if data is undefined', () => {
+            const data = undefined;
+            const spy = spyOn(socketTest, 'emit');
+            service.send(socketGateway1, event, data);
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(spy).toHaveBeenCalledWith(event);
+        });
 
-    // it('should do nothing if listener does not exists', () => {
-    //     spyOn(service['socketListenersList'], 'indexOf').and.returnValue(Constants.minusOne);
-    //     spyOn(service, 'getSocket').and.returnValue(socketTest);
-    //     const spySplice = spyOn(service['socketListenersList'], 'splice');
-    //     socketTest.off = jasmine.createSpy('off');
-    //     service.removeListener('game', 'placeholder');
-    //     expect(spySplice).not.toHaveBeenCalled();
-    //     expect(socketTest.off).not.toHaveBeenCalled();
-    // });
+        it('should not call emit if socket is undefined', () => {
+            const data = 0;
+            const spy = spyOn(socketTest, 'emit');
+            spyOn(service, 'getSocket').and.returnValue(undefined);
 
-    // it('should do nothing if socket is undefined', () => {
-    //     spyOn(service['socketListenersList'], 'indexOf').and.returnValue(0);
-    //     spyOn(service, 'getSocket').and.returnValue(undefined);
-    //     const spySplice = spyOn(service['socketListenersList'], 'splice');
-    //     socketTest.off = jasmine.createSpy('off');
-    //     service.removeListener('game', 'placeholder');
-    //     expect(spySplice).toHaveBeenCalled();
-    //     expect(socketTest.off).not.toHaveBeenCalled();
-    // });
+            service.send(socketGateway1, event);
+            service.send(socketGateway1, event, data);
+            expect(spy).not.toHaveBeenCalled();
+        });
+
+        it('send should call emit with data when using send', () => {
+            const data = 0;
+            const spy = spyOn(socketTest, 'emit');
+            service.send(socketGateway1, event, data);
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(spy).toHaveBeenCalledWith(event, data);
+        });
+    });
 });
