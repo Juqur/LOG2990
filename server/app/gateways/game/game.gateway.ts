44f5074b4a9b7fdcf0d95635179d161fe471f654
LFSTA LAPTOP <algorix1217@gmail.com>
2023-04-07 00:56:32 -0400

    temporary fix to the open close connection problem

--- server/app/gateways/game/game.gateway.ts
+++ server/app/gateways/game/game.gateway.ts
@@ -2,6 +2,7 @@ import { ChatService } from '@app/services/chat/chat.service';
 import { GameService } from '@app/services/game/game.service';
 import { MongodbService } from '@app/services/mongodb/mongodb.service';
 import { TimerService } from '@app/services/timer/timer.service';
+import { ChatMessage } from '@common/chat-messages';
 import { Injectable } from '@nestjs/common';
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
@@ -70,22 +71,40 @@ export class GameGateway {
             this.timerService.stopTimer(socket.id);
             this.gameService.deleteUserFromGame(socket);
 
+            // temporary interface for highscores
             interface Highscore {
                 playerName: string;
                 time: number;
             }
 
-            // this code will look stupid because we made the structure of highscores stupid and we are too lazy to fix it
-            // const scores: Highscore[] = otherSocketId
-            //     ? this.mongodbService.getMultiplayerScores(level.id)
-            //     : this.mongodbService.getSoloPlayerScores(level.id);
-            const endtime = 50; /* this.timerService.getTime(socket);*/
+            const tempLevelId = 8;
+            // This code will look stupid because we made the structure of highscores stupid and we considered its not worth fixing.
+            const scores: Highscore[] = [];
+            let times: number[] = []; // await this.mongodbService.getTimeMultiArray(tempLevelId);
+            // console.log(times);
+            let names: string[] = []; // await this.mongodbService.getPlayerMultiArray(tempLevelId);
+            // console.log('IM LOOSING MY MIND OVER HERE');
+            // console.log(names);
 
-            const scores: Highscore[] = [
-                { playerName: 'the best', time: 10 },
-                { playerName: 'there is no second best', time: 50 },
-                { playerName: 'the loser', time: 60 },
-            ];
+            if (otherSocketId) {
+                names = await this.mongodbService.getPlayerMultiArray(tempLevelId);
+                times = await this.mongodbService.getTimeMultiArray(tempLevelId);
+            } else {
+                names = await this.mongodbService.getPlayerSoloArray(tempLevelId);
+                times = await this.mongodbService.getTimeSoloArray(tempLevelId);
+            }
+
+            for (let i = 0; i < names.length; i++) {
+                scores.push({ playerName: names[i], time: times[i] } as Highscore);
+            }
+            const endtime = this.timerService.getTime(socket);
+
+            console.log(scores);
+            // const scoreks: Highscore[] = [
+            //     { playerName: 'the best', time: 10 },
+            //     { playerName: 'there is no second best', time: 50 },
+            //     { playerName: 'the loser', time: 60 },
+            // ];
 
             if (endtime < scores[2].time) {
                 scores.push({ playerName: 'the newcomer' /* gameState.playerName*/, time: endtime } as Highscore);
--- server/app/services/mongodb/mongodb.service.ts
+++ server/app/services/mongodb/mongodb.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 import { Level } from 'assets/data/level';
 import mongoose from 'mongoose';
 
+mongoose.set('strictQuery', false); // fixes a warning
+
 /**
  * This service is used communicate to the database.
  *
@@ -28,21 +30,85 @@ export class MongodbService {
     }
 
     /**
+     * This method returns the solo highscores of the specified level.
+     *
+     * @param id the id of the level.
+     * @returns level.timesolo the solo highscores of the specified level.
+     */
+    async getTimeSoloArray(id: number): Promise<number[]> {
+        this.openConnection();
+        await new Promise((f) => setTimeout(f, 1000)); // horrible
+        const level = await levelModel.findOne({ id });
+        this.closeConnection();
+        await new Promise((f) => setTimeout(f, 1000)); // horrible
+        return level.timeSolo;
+    }
+
+    /**
+     * This method returns the solo highscores names of the specified level.
+     *
+     * @param id the id of the level.
+     * @returns the solo highscores names of the specified level.
+     */
+    async getPlayerSoloArray(id: number): Promise<string[]> {
+        this.openConnection();
+        await new Promise((f) => setTimeout(f, 1000)); // horrible
+        const level = await levelModel.findOne({ id });
+        this.closeConnection();
+        await new Promise((f) => setTimeout(f, 1000)); // horrible
+        return level.playerSolo;
+    }
+
+    /**
+     * This method returns the multiplayer highscores of the specified level.
+     *
+     * @param id the id of the level.
+     * @returns level.timesolo the multiplayer highscores of the specified level.
+     */
+    async getTimeMultiArray(id: number): Promise<number[]> {
+        this.openConnection();
+        await new Promise((f) => setTimeout(f, 1000)); // horrible
+        const level = await levelModel.findOne({ id });
+        this.closeConnection();
+        await new Promise((f) => setTimeout(f, 1000)); // horrible
+        return level.timeMulti;
+    }
+
+    /**
+     * This method returns the multiplayer highscores names of the specified level.
+     *
+     * @param id the id of the level.
+     * @returns the multiplayer highscores names of the specified level.
+     */
+    async getPlayerMultiArray(id: number): Promise<string[]> {
+        this.openConnection();
+        await new Promise((f) => setTimeout(f, 1000)); // horrible
+        const level = await levelModel.findOne({ id });
+        this.closeConnection();
+        await new Promise((f) => setTimeout(f, 1000)); // horrible
+        return level.playerMulti;
+    }
+
+    /**
      * This method opens the connection to the mongoDB to allow inserting and manipulating the data inside.
      * The method only opens connection if we aren't connected.
      */
-    private openConnection(): void {
-        if (!mongoose.connection.readyState) {
-            mongoose.connect('mongodb+srv://admin:d5jrnGEteyCCNMcW@log2990.ic11qkn.mongodb.net/?');
+    private async openConnection(): Promise<void> {
+        try {
+            if (!mongoose.connection.readyState) {
+                await mongoose.connect('mongodb+srv://admin:d5jrnGEteyCCNMcW@log2990.ic11qkn.mongodb.net/?');
+            }
+        } catch (error) {
+            console.log(error);
         }
     }
 
     /**
      * This method closes the connection to the mongoDB.
      */
-    private closeConnection(): void {
+    private async closeConnection(): Promise<void> {
         if (mongoose.connection.readyState) {
-            mongoose.disconnect();
+            await mongoose.disconnect();
         }
     }
 }
