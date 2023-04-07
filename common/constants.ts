c20c4f8f48548d6c31b1511152f07ed45ee5d348
LFSTA LAPTOP <algorix1217@gmail.com>
2023-04-07 18:22:10 -0400

    changed default constant location

--- common/constants.ts
+++ common/constants.ts
@@ -1,7 +1,6 @@
 /**
  * @author Louis Félix St-Amour
  */
-
 export namespace Constants {
     // Global constants
     export const minusOne: number = -1;
@@ -24,11 +23,13 @@ export namespace Constants {
     // Page display
     export const levelsPerPage: number = 4;
 
-    // Caroussel
+    // Default
+    export const defaultPlayerSolo: string[] = ['Gontrand G.Marcotte', 'primexlbr123', 'Escargot mobile'];
+    export const defaultTimeSolo: number[] = [60, 65, 70];
+    export const defaultPlayerMulti: string[] = ['Jean-Pierre Laflèche', 'GAYSHIM IMPACT', 'someone'];
+    export const defaultTimeMulti: number[] = [80, 83, 90];
 
     // Level mock values
-    export const timeSolo: number[] = [60, 65, 70];
-    export const timeMulti: number[] = [80, 83, 90];
     export const testXposition: number = 96;
     export const testYposition: number = 74;
 
--- server/app/services/image/image.service.constants.ts
+++
@@ -1,1 +0,0 @@
-export const DEFAULT_TIME_VALUES: number[] = [30, 45, 60];
