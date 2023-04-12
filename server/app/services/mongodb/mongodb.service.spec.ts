0a93268aea8de863833ab85f572a60de0475ada9
A123wq123 <charles.degrandpre@polymtl.ca>
2023-04-12 10:29:57 -0400

    feature/high-score/added mock to level and levelModel

--- server/app/services/mongodb/mongodb.service.spec.ts
+++ server/app/services/mongodb/mongodb.service.spec.ts
@@ -1,23 +1,65 @@
-import { Level, levelModel } from '@app/model/schema/level.schema';
-import { getModelToken } from '@nestjs/mongoose';
+import { Level, LevelDocument, levelSchema } from '@app/model/schema/level.schema';
+import { Constants } from '@common/constants';
+import { MongooseModule, getConnectionToken, getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Connection, Model } from 'mongoose';
 import { MongodbService } from './mongodb.service';
+
+const mockLevel = (
+    id = 1,
+    name = 'Juan Cena',
+    playerSolo = ['Bob', 'Charlie', 'Dave'],
+    timeSolo = Constants.defaultTimeSolo,
+    playerMulti = ['The', 'Under', 'Taker'],
+    timeMulti = Constants.defaultTimeMulti,
+    isEasy = true,
+    nbDifferences = Constants.MIN_DIFFERENCES_LIMIT,
+    canJoin = true,
+): Level => ({
+    id,
+    name,
+    playerSolo,
+    timeSolo,
+    playerMulti,
+    timeMulti,
+    isEasy,
+    nbDifferences,
+    canJoin,
+});
+
+const mockLevelDoc = ({})
 
 describe('MongodbService', () => {
     let service: MongodbService;
+    let levelModel: Model<LevelDocument>;
+    let mongoServer: MongoMemoryServer;
+    let connection: Connection;
 
     beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [
-                MongodbService,
-                {
-                    provide: getModelToken(Level.name),
-                    useValue: levelModel,
-                },
-            ],
-        }).compile();
+        mongoServer = await MongoMemoryServer.create();
 
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                MongooseModule.forRootAsync({
+                    useFactory: () => ({
+                        uri: mongoServer.getUri(),
+                    }),
+                }),
+                MongooseModule.forFeature([{ name: Level.name, schema: levelSchema }]),
+            ],
+            providers: [MongodbService],
+        }).compile();
         service = module.get<MongodbService>(MongodbService);
+        levelModel = module.get<Model<LevelDocument>>(getModelToken(Level.name));
+        connection = await module.get(getConnectionToken());
+
+        await levelModel.deleteMany({});
+    });
+
+    afterEach(async () => {
+        await connection.close();
+        await mongoServer.stop();
     });
 
     it('should be defined', () => {
@@ -26,8 +68,8 @@ describe('MongodbService', () => {
 
     it('should return the solo highscores names of the specified level', async () => {
         const levelId = 1;
-        const playerSolo = ['Bob', 'Charlie', 'Dave'];
-        jest.spyOn(levelModel, 'findOne').mockReturnValue({ playerSolo }); // wtf do you mean its a mock of the return value,
+        const playerSolo = { playerSolo: ['Bob', 'Charlie', 'Dave'] };
+        jest.spyOn(levelModel, 'findOne').mockReturnValue(playerSolo); // wtf do you mean its a mock of the return value,
         // why do you want a query?
 
         const result = await service.getPlayerSoloArray(levelId);
