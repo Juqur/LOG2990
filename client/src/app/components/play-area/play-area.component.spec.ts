5626c5447962721cd04a1b37eef9430e8e641a3c
Primexus <pierre.tran@ymail.com>
2023-04-18 14:38:08 -0400

    Fixed test error.

--- client/src/app/components/play-area/play-area.component.spec.ts
+++ client/src/app/components/play-area/play-area.component.spec.ts
@@ -1,30 +1,31 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, fakeAsync, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { CanvasSharingService } from '@app/services/canvas-sharing/canvas-sharing.service';
 import { DrawService } from '@app/services/draw/draw.service';
 import { Constants } from '@common/constants';
-import { environment } from 'src/environments/environment';
 import SpyObj = jasmine.SpyObj;
 
 describe('PlayAreaComponent', () => {
-    let drawServiceSpy: SpyObj<DrawService>;
     let component: PlayAreaComponent;
     let fixture: ComponentFixture<PlayAreaComponent>;
 
-    beforeEach(() => {
-        drawServiceSpy = jasmine.createSpyObj('DrawService', ['drawError', 'drawSuccess', 'drawPlayArea']);
-    });
+    let canvasSharingServiceSpy: SpyObj<CanvasSharingService>;
+    let drawServiceSpy: SpyObj<DrawService>;
 
-    beforeEach(waitForAsync(() => {
+    beforeEach(async () => {
+        drawServiceSpy = jasmine.createSpyObj('DrawService', ['drawError', 'drawSuccess', 'drawPlayArea']);
+        canvasSharingServiceSpy = jasmine.createSpyObj('CanvasSharingService', ['getCanvas', 'setCanvas']);
+
         TestBed.configureTestingModule({
             declarations: [PlayAreaComponent],
             imports: [HttpClientModule],
-        })
-            .overrideProvider(DrawService, { useValue: drawServiceSpy })
-            .compileComponents();
-    }));
+            providers: [
+                { provide: DrawService, useValue: drawServiceSpy },
+                { provide: CanvasSharingService, useValue: canvasSharingServiceSpy },
+            ],
+        }).compileComponents();
 
-    beforeEach(() => {
         fixture = TestBed.createComponent(PlayAreaComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -47,7 +48,7 @@ describe('PlayAreaComponent', () => {
 
     it('drawPlayArea should call context.drawImage', fakeAsync(() => {
         const drawImageSpy = spyOn(CanvasRenderingContext2D.prototype, 'drawImage');
-        component.drawPlayArea(environment.serverUrl + 'original/1.bmp');
+        component.drawPlayArea('');
         component['currentImage'].dispatchEvent(new Event('load'));
 
         expect(drawImageSpy).toHaveBeenCalledTimes(1);
@@ -56,7 +57,7 @@ describe('PlayAreaComponent', () => {
     it('drawPlayArea should call the diff canvas when isDifferenceCanvas is true', fakeAsync(() => {
         const drawImageSpy = spyOn(CanvasRenderingContext2D.prototype, 'drawImage');
         component.isDifferenceCanvas = true;
-        component.drawPlayArea(environment.serverUrl + 'originals/1.bmp');
+        component.drawPlayArea('');
         component['currentImage'].dispatchEvent(new Event('load'));
 
         expect(drawImageSpy).toHaveBeenCalledTimes(1);
