de701260aadb89122d5c0cabe3e7cf5ef6e7f166
Galen Hu <galenhu@hotmail.com>
2023-04-17 21:40:54 -0400

    added mocked thing for video timer

--- client/src/app/components/video-timer/video-timer.component.spec.ts
+++ client/src/app/components/video-timer/video-timer.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VideoTimerComponent } from './video-timer.component';
 
 describe('VideoTimerComponent', () => {
-  let component: VideoTimerComponent;
-  let fixture: ComponentFixture<VideoTimerComponent>;
+    let component: VideoTimerComponent;
+    let fixture: ComponentFixture<VideoTimerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ VideoTimerComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [VideoTimerComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(VideoTimerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(VideoTimerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/components/video-timer/video-timer.component.ts
+++ client/src/app/components/video-timer/video-timer.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Constants } from '@common/constants';
 
 @Component({
-  selector: 'app-video-timer',
-  templateUrl: './video-timer.component.html',
-  styleUrls: ['./video-timer.component.scss']
+    selector: 'app-video-timer',
+    templateUrl: './video-timer.component.html',
+    styleUrls: ['./video-timer.component.scss'],
 })
 export class VideoTimerComponent implements OnInit {
+    gameTimeFormatted: string;
 
-  constructor() { }
+    /**
+     * This method verifies if we aren't connected to the time gateway and connects us to it
+     * if that was the case. We then send the corresponding soloClassic event and prepare to
+     * listen on any 'timer' event for which we want to update the timer value.
+     */
+    ngOnInit(): void {
+        this.updateTimer(0);
+    }
 
-  ngOnInit(): void {
-  }
+    /**
+     * Sets the timer to the given value.
+     * Formats the time into a MM:SS format.
+     *
+     * @param time The time to set the timer to.
+     */
+    updateTimer(time: number): void {
+        const minutes: number = Math.floor(time / Constants.SECONDS_PER_MINUTE);
+        const seconds: number = time % Constants.SECONDS_PER_MINUTE;
 
+        const minutesString: string = minutes < Constants.PADDING_NUMBER ? '0' + minutes : minutes.toString();
+        const secondsString: string = seconds < Constants.PADDING_NUMBER ? '0' + seconds : seconds.toString();
+        this.gameTimeFormatted = 'Temps: ' + minutesString + ':' + secondsString;
+    }
 }
