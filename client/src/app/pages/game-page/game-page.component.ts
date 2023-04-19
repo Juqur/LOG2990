f61514cdc0a42b532d05d74abdf910c03b6c5ca3
Primexus <pierre.tran@ymail.com>
2023-04-19 12:31:06 -0400

    Manual resolution.

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -67,7 +67,7 @@ export class GamePageComponent implements OnInit, OnDestroy {
         if ((event.key === 't' || event.key === 'T') && (event.target as HTMLElement).tagName !== 'TEXTAREA') {
             if (!this.isInCheatMode) {
                 this.socketHandler.send('game', 'onStartCheatMode');
-                this.gamePageService.setPlayArea(this.originalPlayArea, this.differencePlayArea, this.tempDiffPlayArea);
+                this.gamePageService.setPlayArea(this.originalPlayArea, this.differencePlayArea, this.tempDifferencePlayArea);
                 this.gamePageService.setImages(this.levelId);
                 this.isInCheatMode = !this.isInCheatMode;
                 return;
@@ -132,7 +132,7 @@ export class GamePageComponent implements OnInit, OnDestroy {
             }
             if (this.isClassic || gameData.differencePixels.length === 0) {
                 this.gamePageService.setImages(this.levelId);
-                this.gamePageService.setPlayArea(this.originalPlayArea, this.differencePlayArea, this.tempDiffPlayArea);
+                this.gamePageService.setPlayArea(this.originalPlayArea, this.differencePlayArea, this.tempDifferencePlayArea);
                 const isFound = this.gamePageService.handleResponse(this.isInCheatMode, gameData, this.clickedOriginalImage);
                 if (isFound && this.showThirdHint) {
                     this.removeHintShape();
@@ -158,7 +158,7 @@ export class GamePageComponent implements OnInit, OnDestroy {
         this.socketHandler.on('game', 'hintRequest', (data) => {
             const section = data as number[];
             this.gamePageService.setImages(this.levelId);
-            this.gamePageService.setPlayArea(this.originalPlayArea, this.differencePlayArea, this.tempDiffPlayArea);
+            this.gamePageService.setPlayArea(this.originalPlayArea, this.differencePlayArea, this.tempDifferencePlayArea);
             if (section.length < 3 && this.nbHints > 1) {
                 this.gamePageService.handleHintRequest(section);
                 this.nbHints--;
@@ -212,7 +212,7 @@ export class GamePageComponent implements OnInit, OnDestroy {
      */
     askForHint(): void {
         if (!this.secondPlayerName) {
-            this.gamePageService.setPlayArea(this.originalPlayArea, this.differencePlayArea, this.tempDiffPlayArea);
+            this.gamePageService.setPlayArea(this.originalPlayArea, this.differencePlayArea, this.tempDifferencePlayArea);
             this.socketHandler.send('game', 'onHintRequest');
         }
     }
